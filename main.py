c9b2887c7401a96335a3e080f124203479c4cd27
Mohammed Ghaith Altarabichi <ghaith81981@hotmail.com>
2021-06-29 00:32:26 +0200

    First commit.

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('hello')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
